0b833353e88cb03a4723700d83bd5dff7ced510f
unknown <kvadam@gmail.com>
2022-08-24 12:21:19 +0200

    Site data copied.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                "hero-image": "url('./pexels-markus-spiske-330771.jpg')",
+            },
+        },
     },
     plugins: [],
 };
